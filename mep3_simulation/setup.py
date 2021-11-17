bfcd041442c635444476a9cf731985da447cb8bf
Filip Parag <filip@parag.rs>
2021-11-17 17:16:59 +0100

    ros2 creating a custom package tutorial

---
+++ mep3_simulation/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'mep3_simulation'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='filiparag',
+    maintainer_email='filiparag@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
