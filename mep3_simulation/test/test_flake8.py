881aa38e10aa14023f5748164df6beb8e9c8a47c
Filip Parag <filip@parag.rs>
2021-12-02 12:03:20 +0100

    modify flake8 rules

--- mep3_simulation/test/test_flake8.py
+++ mep3_simulation/test/test_flake8.py
@@ -19,7 +19,9 @@ import pytest
 @pytest.mark.flake8
 @pytest.mark.linter
 def test_flake8():
-    rc, errors = main_with_errors(argv=[])
+    rc, errors = main_with_errors(argv=[
+        '--linelength', '100'
+    ])
     assert rc == 0, \
         'Found %d code style errors / warnings:\n' % len(errors) + \
         '\n'.join(errors)
