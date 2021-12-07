7014a120f1fa09065417cde2dd21a719c4807253
Filip Parag <filip@parag.rs>
2021-12-07 21:03:03 +0100

    Continuous integration (#29)
    Co-authored-by: Darko Lukic <lukicdarkoo@gmail.com>

--- mep3_driver/launch/driver_launch.py
+++ mep3_driver/launch/driver_launch.py
@@ -1,19 +1,22 @@
 import os
 import pathlib
-from launch import LaunchDescription
+
 from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
     package_dir = get_package_share_directory('mep3_driver')
 
-    robot_description = pathlib.Path(os.path.join(package_dir, 'resource', 'mep3_big_config.urdf')).read_text()
-    ros2_control_params = os.path.join(package_dir, 'resource', 'mep3_big_ros2_control.yaml')
+    robot_description = pathlib.Path(os.path.join(
+        package_dir, 'resource', 'mep3_big_config.urdf')).read_text()
+    ros2_control_params = os.path.join(
+        package_dir, 'resource', 'mep3_big_ros2_control.yaml')
 
     controller_manager_node = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
+        package='controller_manager',
+        executable='ros2_control_node',
         parameters=[
             {'robot_description': robot_description},
             ros2_control_params
--- mep3_simulation/launch/robot_launch.py
+++ mep3_simulation/launch/robot_launch.py
@@ -1,11 +1,12 @@
 import os
 import pathlib
+
+from ament_index_python.packages import get_package_share_directory
 import launch
-from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 from webots_ros2_core.webots_launcher import WebotsLauncher
 
 
@@ -41,11 +42,12 @@ def generate_launch_description():
         arguments=['joint_state_broadcaster'] + controller_manager_timeout,
     )
 
-    # The node which interacts with a robot in the Webots simulation is located in the
-    # `webots_ros2_driver` package under name `driver`.
+    # The node which interacts with a robot in the Webots simulation is located
+    # in the `webots_ros2_driver` package under name `driver`.
     # It is necessary to run such a node for each robot in the simulation.
-    # Typically, we provide it the `robot_description` parameters from a URDF file and
-    # `ros2_control_params` from the `ros2_control` configuration file.
+    # Typically, we provide it the `robot_description` parameters from a URDF
+    # file and `ros2_control_params` from the `ros2_control`
+    # configuration file.
     webots_robot_driver = Node(
         package='webots_ros2_driver',
         executable='driver',
@@ -58,13 +60,13 @@ def generate_launch_description():
         ]
     )
 
-    # Often we want to publish robot transforms, so we use the `robot_state_publisher`
-    # node for that.
-    # If robot model is not specified in the URDF file then Webots can help us with the
-    # URDF exportation feature.
-    # Since the exportation feature is available only once the simulation has started and
-    # the `robot_state_publisher` node requires a `robot_description` parameter before we
-    # have to specify a dummy robot.
+    # Often we want to publish robot transforms, so we use the
+    # `robot_state_publisher` node for that.
+    # If robot model is not specified in the URDF file then Webots can help
+    # us with the URDF exportation feature.
+    # Since the exportation feature is available only once the simulation has
+    # started and the `robot_state_publisher` node requires a
+    # `robot_description` parameter before we have to specify a dummy robot.
     robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
