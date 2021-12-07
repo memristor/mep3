f5da4f81573632dbe435fe6c7df643e01174ebad
Filip Parag <filip@parag.rs>
2021-12-07 21:23:16 +0100

    load tree from file

--- mep3_planning/src/mep3_planning.cpp
+++ mep3_planning/src/mep3_planning.cpp
@@ -1,84 +1,44 @@
 #include <cstdio>
 #include "behaviortree_cpp_v3/bt_factory.h"
-
-#ifdef MANUAL_STATIC_LINKING
 #include "mep3_planning/dummy_nodes.hpp"
-#endif
+
+#include <filesystem>
+#include <iostream>
 
 using namespace BT;
 
-/** Behavior Tree are used to create a logic to decide what
- * to "do" and when. For this reason, our main building blocks are
- * Actions and Conditions.
- *
- * In this tutorial, we will learn how to create custom ActionNodes.
- * It is important to remember that NodeTree are just a way to
- * invoke callbacks (called tick() ). These callbacks are implemented by the user.
- */
-
-// clang-format off
-static const char* xml_text = R"(
- <root main_tree_to_execute = "MainTree" >
-     <BehaviorTree ID="MainTree">
-        <Sequence name="root_sequence">
-            <CheckBattery   name="battery_ok"/>
-            <OpenGripper    name="open_gripper"/>
-            <ApproachObject name="approach_object"/>
-            <CloseGripper   name="close_gripper"/>
-        </Sequence>
-     </BehaviorTree>
- </root>
- )";
-
-
 int main(int argc, char **argv)
 {
-  (void)argc;
-  (void)argv;
+    (void)argc;
+    (void)argv;
 
-  // We use the BehaviorTreeFactory to register our custom nodes
-    BehaviorTreeFactory factory;
+    if (argc < 2)
+    {
+        std::cerr << "Error: Missing argument: strategy name" << std::endl;
+        return 1;
+    }
 
-    /* There are two ways to register nodes:
-    *    - statically, i.e. registering all the nodes one by one.
-    *    - dynamically, loading the TreeNodes from a shared library (plugin).
-    * */
+    auto tree_file = (std::filesystem::path(ASSETS_DIRECTORY) / "strategies" / argv[1]).replace_extension(".xml");
 
-#ifdef MANUAL_STATIC_LINKING
-    // Note: the name used to register should be the same used in the XML.
-    // Note that the same operations could be done using DummyNodes::RegisterNodes(factory)
+    if (!std::filesystem::exists(tree_file))
+    {
+        std::cerr << "Strategy file " << tree_file << " does not exist" << std::endl;
+        return 1;
+    }
 
     using namespace DummyNodes;
 
-    // The recommended way to create a Node is through inheritance.
-    // Even if it requires more boilerplate, it allows you to use more functionalities
-    // like ports (we will discuss this in future tutorials).
+    BehaviorTreeFactory factory;
     factory.registerNodeType<ApproachObject>("ApproachObject");
 
-    // Registering a SimpleActionNode using a function pointer.
-    // you may also use C++11 lambdas instead of std::bind
     factory.registerSimpleCondition("CheckBattery", std::bind(CheckBattery));
 
-    //You can also create SimpleActionNodes using methods of a class
     GripperInterface gripper;
+
     factory.registerSimpleAction("OpenGripper", std::bind(&GripperInterface::open, &gripper));
     factory.registerSimpleAction("CloseGripper", std::bind(&GripperInterface::close, &gripper));
 
-#else
-    // Load dynamically a plugin and register the TreeNodes it contains
-    // it automated the registering step.
-    factory.registerFromPlugin("./libdummy_nodes_dyn.so");
-#endif
-
-    // Trees are created at deployment-time (i.e. at run-time, but only once at the beginning).
-    // The currently supported format is XML.
-    // IMPORTANT: when the object "tree" goes out of scope, all the TreeNodes are destroyed
-    auto tree = factory.createTreeFromText(xml_text);
-
-    // To "execute" a Tree you need to "tick" it.
-    // The tick is propagated to the children based on the logic of the tree.
-    // In this case, the entire sequence is executed, because all the children
-    // of the Sequence return SUCCESS.
+    auto tree = factory.createTreeFromFile(tree_file);
     tree.tickRoot();
 
     return 0;
