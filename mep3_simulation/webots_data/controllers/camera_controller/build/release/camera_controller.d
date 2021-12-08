ba7989d755cf8a0ace1c506878ed08809d23287e
Vladimir Vincan <vladimirvincan@gmail.com>
2021-12-08 04:44:09 +0100

    added raspicam

---
+++ mep3_simulation/webots_data/controllers/camera_controller/build/release/camera_controller.d
@@ -0,0 +1,6 @@
+build/release/camera_controller.o: camera_controller.cpp \
+ /usr/local/webots/include/controller/cpp/webots/Robot.hpp \
+ /usr/local/webots/include/controller/cpp/webots/Camera.hpp \
+ /usr/local/webots/include/controller/cpp/webots/Device.hpp \
+ /usr/local/webots/include/controller/cpp/webots/../../c/webots/types.h \
+ /usr/local/webots/include/controller/cpp/webots/../../c/webots/camera_recognition_object.h
