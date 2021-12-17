0ac6d4954cf8dde0b2f77b3e0bc61b0a19477516
Filip Parag <filip@parag.rs>
2021-12-17 17:17:33 +0100

    wip refactor to fmrico/master_ros2

---
+++ mep3_planning/include/mep3_planning/navigate_to_pose.hpp
@@ -0,0 +1,44 @@
+#ifndef MR2_BEHAVIOR_TREES__MOVE_HPP_
+#define MR2_BEHAVIOR_TREES__MOVE_HPP_
+
+#include <string>
+
+#include "mep3_planning/bt_action_node.hpp"
+#include "behaviortree_cpp_v3/behavior_tree.h"
+#include "behaviortree_cpp_v3/bt_factory.h"
+
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "nav2_msgs/action/navigate_to_pose.hpp"
+
+namespace BT
+{
+    struct Pose2D
+    {
+        double x, y, theta;
+    };
+}
+
+namespace mep3_planning
+{
+
+    class NavigateToPose : public mep3_planning::BtActionNode<nav2_msgs::action::NavigateToPose>
+    {
+    public:
+        explicit NavigateToPose(
+            const std::string &xml_tag_name,
+            const std::string &action_name,
+            const BT::NodeConfiguration &conf);
+
+        void on_tick() override;
+        BT::NodeStatus on_success() override;
+
+        static BT::PortsList providedPorts()
+        {
+            return {
+                BT::InputPort<geometry_msgs::msg::PoseStamped>("goal")};
+        }
+    };
+
+}
+
+#endif
--- mep3_planning/src/mep3_planning.cpp
+++ mep3_planning/src/mep3_planning.cpp
@@ -1,14 +1,14 @@
 #include "rclcpp/rclcpp.hpp"
 #include "behaviortree_cpp_v3/bt_factory.h"
 #include "behaviortree_cpp_v3/loggers/bt_cout_logger.h"
-#include "mep3_planning/navigate_to_action.hpp"
-#include "mep3_planning/behavior_tree_ros_node.hpp"
+#include "mep3_planning/navigate_to_pose.hpp"
+// #include "mep3_planning/behavior_tree_ros_node.hpp"
 
 #include <cstdio>
 #include <filesystem>
 #include <iostream>
 
-mep3_planning::BehaviorTreeRosNode::BehaviorTreeRosNode() : rclcpp::Node("behavior_tree_ros_node") {}
+// mep3_planning::BehaviorTreeRosNode::BehaviorTreeRosNode() : rclcpp::Node("behavior_tree_ros_node") {}
 
 namespace BT
 {
@@ -51,20 +51,46 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    // Create a behavior tree
+    auto node = rclcpp::Node::make_shared("navigate_to_pose");
+
     BT::BehaviorTreeFactory factory;
-    factory.registerNodeType<NavigateToAction>("NavigateToAction");
-    auto tree = factory.createTreeFromFile(tree_file);
-    BT::StdCoutLogger logger_cout(tree);
+    // BT::SharedLibrary loader;
 
-    // Pass the ROS node to Behavior Tree nodes
-    rclcpp::init(argc, argv);
-    mep3_planning::BehaviorTreeRosNode::SharedPtr behavior_tree_ros_node = std::make_shared<mep3_planning::BehaviorTreeRosNode>();
-    for (auto &node : tree.nodes)
-        if (mep3_planning::InitInterface *behavior_tree_node = dynamic_cast<mep3_planning::InitInterface *>(node.get()))
-            behavior_tree_node->init(behavior_tree_ros_node);
-    tree.tickRoot();
+    // factory.registerFromPlugin(loader.getOSName("mr2_battery_checker_bt_node"));
+    // factory.registerFromPlugin(loader.getOSName("mr2_patrol_bt_node"));
+    // factory.registerFromPlugin(loader.getOSName("mr2_recharge_bt_node"));
+    // factory.registerFromPlugin(loader.getOSName("mr2_move_bt_node"));
+    // factory.registerFromPlugin(loader.getOSName("mr2_get_waypoint_bt_node"));
+
+    auto blackboard = BT::Blackboard::create();
+    blackboard->set("node", node);
+    BT::Tree tree = factory.createTreeFromFile(tree_file, blackboard);
+
+    bool finish = false;
+    while (!finish && rclcpp::ok())
+    {
+        finish = tree.rootNode()->executeTick() == BT::NodeStatus::SUCCESS;
+
+        rclcpp::spin_some(node);
+    }
+
     rclcpp::shutdown();
-
     return 0;
+
+    // // Create a behavior tree
+    // BT::BehaviorTreeFactory factory;
+    // factory.registerNodeType<NavigateToAction>("NavigateToAction");
+    // auto tree = factory.createTreeFromFile(tree_file);
+    // BT::StdCoutLogger logger_cout(tree);
+
+    // // Pass the ROS node to Behavior Tree nodes
+    // rclcpp::init(argc, argv);
+    // mep3_planning::BehaviorTreeRosNode::SharedPtr behavior_tree_ros_node = std::make_shared<mep3_planning::BehaviorTreeRosNode>();
+    // for (auto &node : tree.nodes)
+    //     if (mep3_planning::InitInterface *behavior_tree_node = dynamic_cast<mep3_planning::InitInterface *>(node.get()))
+    //         behavior_tree_node->init(behavior_tree_ros_node);
+    // tree.tickRoot();
+    // rclcpp::shutdown();
+
+    // return 0;
 }
