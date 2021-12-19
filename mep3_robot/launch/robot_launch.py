29d7651e6ad05039afb8392d3fedcea6dddce538
Filip Parag <filip@parag.rs>
2021-12-19 18:53:18 +0100

    robot lint

--- mep3_robot/launch/robot_launch.py
+++ mep3_robot/launch/robot_launch.py
@@ -1,5 +1,7 @@
 import os
+
 from ament_index_python.packages import get_package_share_directory
+
 import launch
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
