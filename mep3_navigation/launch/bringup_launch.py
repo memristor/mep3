02ec9ce5a2c73b9d5147756bc9f388adfda99993
Filip Parag <filip@parag.rs>
2022-01-04 18:51:05 +0100

    linter fix

--- mep3_navigation/launch/bringup_launch.py
+++ mep3_navigation/launch/bringup_launch.py
@@ -21,7 +21,7 @@ from launch.actions import (DeclareLaunchArgument, GroupAction,
                             IncludeLaunchDescription, SetEnvironmentVariable)
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import PushRosNamespace
 from launch_ros.actions import Node
 from nav2_common.launch import RewrittenYaml
