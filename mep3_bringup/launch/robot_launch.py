9d17f52a026a9c391854d198948bc7982b770d49
Darko Lukic <lukicdarkoo@gmail.com>
2022-01-14 01:01:07 +0100

    change nav name

--- mep3_bringup/launch/robot_launch.py
+++ mep3_bringup/launch/robot_launch.py
@@ -46,7 +46,7 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(os.path.join(
             get_package_share_directory('mep3_navigation'),
             'launch',
-            'bringup_launch.py'
+            'navigation_launch.py'
         )),
         launch_arguments=[
             ('map', nav2_map),
