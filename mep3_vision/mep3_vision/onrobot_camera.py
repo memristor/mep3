6689b5bb9f078f6f9b935f87915d47f13a9eab1c
Milos Arbanas <milostravian2@gmail.com>
2022-01-25 17:36:34 +0100

    Using .yaml calibration file that is generated by image_pipeline package

--- mep3_vision/mep3_vision/onrobot_camera.py
+++ mep3_vision/mep3_vision/onrobot_camera.py
@@ -11,8 +11,8 @@ from rclpy.node import Node  # Handles the creation of nodes
 from sensor_msgs.msg import Image  # Image is the message type
 from cv_bridge import CvBridge  # Package to convert between ROS and OpenCV Images
 import os
-import pickle
-
+import yaml
+import numpy as np
 
 
 class ImageSubscriber(Node):
@@ -29,17 +29,17 @@ class ImageSubscriber(Node):
 
         # TODO: Calibration. Now it's just the Dummy calibration file...
         if True:
-            f = open('/home/milos/foxy_ws/src/mep3/mep3_vision/mep3_vision/DummyCameraCalibration.pckl', 'rb')
-            u = pickle._Unpickler(f)
-            u.encoding = 'latin1'
-            (x, y, _, _) = u.load()
-            self.cameraMatrix = x
-            self.distCoeffs = y
-            f.close()
-            if self.cameraMatrix is None or self.distCoeffs is None:
-                print(
-                    "Calibration issue. Remove ./calibration/CameraCalibration.pckl and recalibrate your camera with calibration_ChAruco.py.")
-                exit()
+            with open("src/mep3/mep3_vision/mep3_vision/ost.yaml", "r") as stream:
+            # with open("ost.yaml", "r") as stream:
+                try:
+                    camera_data = yaml.safe_load(stream)
+                except yaml.YAMLError as exc:
+                    print(exc)
+
+                self.cameraMatrix = np.array(camera_data['camera_matrix']['data'])
+                self.distCoeffs = np.array(camera_data['distortion_coefficients']['data'])
+                self.cameraMatrix = self.cameraMatrix.reshape(3, 3)
+                self.distCoeffs = self.distCoeffs.reshape(1, 5)
 
         # Create the subscriber. This subscriber will receive an Image
         # from the video_frames topic. The queue size is 10 messages.
@@ -74,13 +74,23 @@ class ImageSubscriber(Node):
 
 
             rotation_vectors, translation_vectors, _objPoints = cv2.aruco.estimatePoseSingleMarkers(corners, 1,
-                                                                                                self.cameraMatrix,
-                                                                                                self.distCoeffs)
+                                                                                                    self.cameraMatrix,
+                                                                                                    self.distCoeffs)
 
-
-
+            cv2.aruco.drawDetectedMarkers(current_frame, corners, ids)
+            i = 0
             for rvec, tvec in zip(rotation_vectors, translation_vectors):
                 cv2.aruco.drawAxis(current_frame, self.cameraMatrix, self.distCoeffs, rvec, tvec, 1)
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                # cv2.putText(current_frame, 'Christmas', (int(corners[i][0][0][0], int(corners[i][0][0][1]))),
+                #             font, 4,(255,255,255),2,cv2.LINE_AA)
+                print((int(corners[i][0][0][0]), int(corners[i][0][0][1])))
+                cv2.putText(current_frame, "{:.3f} {:.3f}".format(translation_vectors[0][0][:2][0],
+                                                                  translation_vectors[0][0][:2][1]
+                                                                  ), (int(corners[i][0][0][0]), int(corners[i][0][0][1])),
+                            font, 1, (0, 128, 128), 2, cv2.LINE_AA)
+
+                i+= 1
                 print("Translation vector: ", tvec)
 
         # Display image
