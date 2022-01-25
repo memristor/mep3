ffb3690f752675f9b7bfaae57f9652bf02cd641d
Filip Parag <filip@parag.rs>
2022-01-26 00:13:57 +0100

    use mep3_msgs

--- mep3_behavior_tree/include/mep3_behavior_tree/distance_angle_action.hpp
+++ mep3_behavior_tree/include/mep3_behavior_tree/distance_angle_action.hpp
@@ -18,7 +18,8 @@
 #include <string>
 
 #include "mep3_behavior_tree/bt_action_node.hpp"
-#include "mep3_navigation/distance_angle_regulator.hpp"
+#include "mep3_msgs/action/motion_command.hpp"
+#include "mep3_behavior_tree/pose_2d.hpp"
 
 #include "behaviortree_cpp_v3/behavior_tree.h"
 #include "behaviortree_cpp_v3/bt_factory.h"
@@ -26,12 +27,16 @@
 namespace mep3_behavior_tree
 {
 
-    class DistanceAngleAction : public mep3_behavior_tree::BtActionNode<DistanceAngleRegulator>
+    class DistanceAngleAction : public mep3_behavior_tree::BtActionNode<mep3_msgs::action::MotionCommand>
     {
     public:
         explicit DistanceAngleAction(
             const std::string &name,
-            const BT::NodeConfiguration &config)
+            const BT::NodeConfiguration &config) :
+            mep3_behavior_tree::BtActionNode<mep3_msgs::action::MotionCommand>(
+                  name,
+                  config,
+                  "navigate_to_pose")
         {
         }
 
@@ -41,7 +46,8 @@ namespace mep3_behavior_tree
         static BT::PortsList providedPorts()
         {
             return {
-                BT::InputPort<BT::Pose2D>("goal")};
+                BT::InputPort<BT::Pose2D>("goal")
+            };
         }
     };
 
