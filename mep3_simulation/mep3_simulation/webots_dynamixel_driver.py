d89f79d0f4b65b9e2533650b500eaf63e708c51d
vladimirvincan <vladimirvincan@gmail.com>
2022-02-02 22:23:41 +0100

    timeout works

--- mep3_simulation/mep3_simulation/webots_dynamixel_driver.py
+++ mep3_simulation/mep3_simulation/webots_dynamixel_driver.py
@@ -1,26 +1,27 @@
-import time
 from math import radians
+import time
+
+from mep3_msgs.action import DynamixelCommand
 import rclpy
-import rclpy.node
 from rclpy.action import ActionServer, CancelResponse, GoalResponse
 from rclpy.callback_groups import ReentrantCallbackGroup
 from rclpy.executors import MultiThreadedExecutor
-from mep3_msgs.action import DynamixelCommand
+import rclpy.node
+
+DEFAULT_POSITION = radians(0)  # deg
+DEFAULT_VELOCITY = radians(45)  # deg/s
+DEFAULT_TOLERANCE = radians(1)  # deg
+DEFAULT_TIMEOUT = 5  # s
+"""
+Test:
+ros2 action send_goal /big/dynamixel_command/arm_right_motor_base mep3_msgs/action/DynamixelCommand "position: 2.2"  # noqa: E501
+"""
 
 
-DEFAULT_POSITION = radians(0) # deg
-DEFAULT_VELOCITY = radians(45) # deg/s
-DEFAULT_TOLERANCE = radians(1) # deg
-DEFAULT_TIMEOUT = 5 # s
-
-
-# Test: ros2 action send_goal /big/dynamixel_command/arm_right_motor_base mep3_msgs/action/DynamixelCommand "position: 2.2"
 class WebotsDynamixelDriver:
+
     def init(self, webots_node, properties):
-        try:
-            rclpy.init(args=None)
-        except Exception as e:
-            pass
+        rclpy.init(args=None)
         self.__executor = MultiThreadedExecutor()
 
         namespace = properties['namespace']
@@ -41,12 +42,9 @@ class WebotsDynamixelDriver:
             execute_callback=self.__execute_callback,
             callback_group=ReentrantCallbackGroup(),
             goal_callback=self.__goal_callback,
-            cancel_callback=self.__cancel_callback
-        )
+            cancel_callback=self.__cancel_callback)
 
     def __timeout_overflow(self, timeout):
-        print("curr time: ", self.__robot.getTime())
-        print("start time:", self.__start_time)
         if self.__robot.getTime() - self.__start_time > timeout:
             return True
         else:
@@ -78,14 +76,14 @@ class WebotsDynamixelDriver:
             if self.__timeout_overflow(timeout):
                 # Return failure
                 result.result = 1
-                goal_handle.canceled()
-                return DynamixelCommand.Result()
+                goal_handle.abort()
+                return result
 
             if goal_handle.is_cancel_requested:
                 # Return failure
                 result.result = 2
                 goal_handle.canceled()
-                return DynamixelCommand.Result()
+                return result
 
             time.sleep(0.1)
 
