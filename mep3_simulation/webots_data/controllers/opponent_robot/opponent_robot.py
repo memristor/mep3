ad40548cd069246f1d471edd86bcc94a6dab1bcc
Marija Golubovic <marijagolubovic16@gmail.com>
2022-02-04 18:04:32 +0100

    Modify the controller to use it for both robots

--- mep3_simulation/webots_data/controllers/opponent_robot/opponent_robot.py
+++ mep3_simulation/webots_data/controllers/opponent_robot/opponent_robot.py
@@ -14,64 +14,67 @@ name = name_field.getSFString()
 
 position = [0, 0, 0]
 
-positions = [(-1.34, 0.437, 10), (-1.12, 0.706, 10), (-0.761, 0.786, 20),
-             (-0.13, 0.792, 10), (-0.764, 0.53, 10), (-0.848, 0.297, 15),
-             (-0.713, 0.063, 10), (-0.603, -0.127, 15)]
+POSITIONS_1 = [(-1.34, 0.437, 10), (-1.12, 0.706, 10), (-0.761, 0.786, 20),
+               (-0.13, 0.792, 10), (-0.764, 0.53, 10), (-0.848, 0.297, 15),
+               (-0.713, 0.063, 10), (-0.603, -0.127, 15)]
+
+POSITIONS_2 = [(-1.4, 0.0238, 10), (-1.22, -0.257, 10), (-1.13, -0.646, 10),
+               (-0.738, -0.904, 15), (-0.565, -0.666, 20),
+               (-0.827, -0.412, 15)]
 
 
-def destination_achieved(curr, dest, epsilon):
-    return (abs(curr[0] - dest[0]) < epsilon
-            and abs(curr[1] - dest[1]) < epsilon)
+def destination_achieved(current_position, destination_position, epsilon=0.05):
+    return abs(current_position[0] -
+               destination_position[0]) < epsilon and abs(
+                   current_position[1] - destination_position[1]) < epsilon
 
 
-def time_achieved(time_period):
+def is_time_achieved(time_period):
     ret_value = False
-    t = supervisor.getTime()
-    while supervisor.getTime() - t < time_period:
+    current_time = supervisor.getTime()
+
+    while supervisor.getTime() - current_time < time_period:
         supervisor.step(timestep)
-        ret_value = False
     else:
         ret_value = True
 
     return ret_value
 
 
-delta = 0.001
-epsilon = 0.05
+DELTA = 0.001
 achieved_destination = False
+
+positions = POSITIONS_1 if supervisor.getName() == 'opponent_box' else POSITIONS_2
+
 destination = positions[random.randint(0, len(positions) - 1)]
+
 while supervisor.step(timestep) != -1:
 
     current_position = opponent_field.getSFVec3f()
 
-    if (achieved_destination):
+    if achieved_destination:
         destination = random.choice(positions)
 
-    if (destination_achieved(current_position, destination, epsilon)):
+    if (destination_achieved(current_position, destination)):
 
-        if (time_achieved(destination[2])):
-            t = supervisor.getTime()
-
+        if is_time_achieved(destination[2]):
             achieved_destination = True
 
     else:
         achieved_destination = False
         if current_position[0] < destination[0]:
-            current_position[0] += delta
-        if current_position[1] < destination[1]:
-            current_position[1] += delta
-        if current_position[0] > destination[0]:
-            current_position[0] -= delta
-        if current_position[1] > destination[1]:
-            current_position[1] -= delta
-        if (current_position[0] != destination[0]
-                and current_position[1] != destination[1]):
-            opponent_field.setSFVec3f(current_position)
+            current_position[0] += DELTA
+        else:
+            current_position[0] -= DELTA
 
+        if current_position[1] < destination[1]:
+            current_position[1] += DELTA
+        else:
+            current_position[1] -= DELTA
+
+        if current_position[0] != destination[0] and current_position[1] != destination[1]:
+
+            opponent_field.setSFVec3f(current_position)
         else:
             current_position[0] = destination[0]
             current_position[1] = destination[1]
-
-    pass
-
-    # Enter here exit cleanup code.
