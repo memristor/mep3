55703754ad1eaa104695639c7e0be34810cffbd6
Filip Parag <filip@parag.rs>
2022-02-13 21:33:53 +0100

    webots config; remove vscode duplicate extension

--- docker/provision/devel.mk
+++ docker/provision/devel.mk
@@ -2,6 +2,7 @@ webots:
 	wget -nv -O ./webots.deb 'https://github.com/cyberbotics/webots/releases/download/R2022a/webots_2022a_amd64.deb'
 	sudo -E apt-get install -y ./webots.deb
 	rm -f ./webots.deb
+	test -d /memristor/.config/Cyberbotics || rsync -rP ./config/Cyberobotics/ /memristor/.config/Cyberbotics/
 
 groot:
 	wget -nv -O ./Groot.AppImage 'https://github.com/BehaviorTree/Groot/releases/download/1.0.0/Groot-1.0.0-x86_64.AppImage'
@@ -24,5 +25,4 @@ vscode:
 	code --install-extension eamodio.gitlens
 	code --install-extension ms-python.python
 	code --install-extension ms-vscode.cpptools-extension-pack
-	code --install-extension twxs.cmake
 	code --install-extension usernamehw.errorlens
