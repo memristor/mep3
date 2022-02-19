94b572a3d2f1e0ef3a443dea7f20a3314dbb00c8
Dušan Jovanović <angstrem98@gmail.com>
2022-02-19 17:28:07 +0100

    shouldRotateToGoalHeading fix

--- mep3_navigation/src/regulated_pure_pursuit_controller/regulated_pure_pursuit_controller.cpp
+++ mep3_navigation/src/regulated_pure_pursuit_controller/regulated_pure_pursuit_controller.cpp
@@ -475,8 +475,9 @@ bool RegulatedPurePursuitController::shouldRotateToGoalHeading(
   const geometry_msgs::msg::PoseStamped & carrot_pose)
 {
   // Whether we should rotate robot to goal heading
-  double dist_to_goal = std::hypot(carrot_pose.pose.position.x, carrot_pose.pose.position.y);
-  return use_rotate_to_heading_ && dist_to_goal < goal_dist_tol_;
+  double carrot_dist = std::hypot(carrot_pose.pose.position.x, carrot_pose.pose.position.y);
+  return use_rotate_to_heading_ && carrot_dist < goal_dist_tol_ &&
+         fabs(lookahead_dist - carrot_dist) > 2.0 * costmap_->getResolution();
 }
 
 
