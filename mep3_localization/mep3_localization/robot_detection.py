bb8e3a88f7d12374aaad8400bd08a52a68c4d2eb
Vladimir Vincan <vladimirvincan@gmail.com>
2022-02-25 23:57:08 +0100

    pose estimation

--- mep3_localization/mep3_localization/robot_detection.py
+++ mep3_localization/mep3_localization/robot_detection.py
@@ -1,9 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 import cv2
+import numpy as np
+from cv2 import aruco
 from cv_bridge import CvBridge
+"""
+This class receives video from the central RasPi Cam,
+and sends TF of Aruco tags.
+"""
 
 
 class DetectedRobots(Node):
@@ -14,12 +20,83 @@ class DetectedRobots(Node):
             Image, '/cam/cam_central/RasPi0', self.listener_callback, 10)
         self.subscription
 
+        # cv_file = cv2.FileStorage('calibration_camera.yaml', cv2.FILE_STORAGE)
+
         self.br = CvBridge()
+        self.dict = aruco.Dictionary_get(aruco.DICT_4X4_100)
+        self.params = aruco.DetectorParameters_create()
+        # self.cameraMatrix = cv_file.getNode('K').mat()
+        # self.distCoeffs = cv_file.getNode('D').mat()
+        self.cameraMatrix = np.array([[650, 0, 1920 / 2], [0, 650, 1080 / 2],
+                                      [0, 0, 1]])
+        self.distCoeffs = np.zeros((5, 1))
+        # Side length of the ArUco marker in meters
+        self.aruco_marker_side_length = 0.07
+        # cv_file.release()
 
     def listener_callback(self, data):
         self.get_logger().info('Receiving video frame')
-        current_frame = self.br.imgmsg_to_cv2(data)
-        cv2.imshow("camera", current_frame)
+        current_frame = self.br.imgmsg_to_cv2(data, "bgr8")
+        self.draw_aruco_pose(current_frame)
+
+    def draw_aruco_pose(self, frame):
+        (corners, ids,
+         rejected) = aruco.detectMarkers(frame,
+                                         self.dict,
+                                         parameters=self.params,
+                                         cameraMatrix=self.cameraMatrix,
+                                         distCoeff=self.distCoeffs)
+        # Check that at least one ArUco marker was detected
+        if ids is not None:
+            aruco.drawDetectedMarkers(frame, corners, ids)
+            rvecs, tvecs, obj_points = cv2.aruco.estimatePoseSingleMarkers(
+                corners, self.aruco_marker_side_length, self.cameraMatrix,
+                self.distCoeffs)
+
+            print("RVECS:")
+            print(len(rvecs))
+            print(rvecs)
+            print("TVECS:")
+            print(tvecs)
+            for i in range(len(rvecs)):
+                aruco.drawAxis(frame, self.cameraMatrix, self.distCoeffs,
+                               rvecs[i], tvecs[i],
+                               self.aruco_marker_side_length)
+        cv2.imshow("camera", frame)
+        cv2.waitKey(1)
+
+    def draw_aruco_border(self, frame):
+        (corners, ids, rejected) = aruco.detectMarkers(frame,
+                                                       self.dict,
+                                                       parameters=self.params)
+        if len(corners) > 0:
+            ids = ids.flatten()
+            for (markerCorner, markerID) in zip(corners, ids):
+                corners = markerCorner.reshape((4, 2))
+                (topLeft, topRight, bottomRight, bottomLeft) = corners
+
+                # convert each of the (x, y)-coordinate pairs to integers
+                topRight = (int(topRight[0]), int(topRight[1]))
+                bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+                bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+                topLeft = (int(topLeft[0]), int(topLeft[1]))
+
+                cv2.line(frame, topLeft, topRight, (0, 255, 0), 2)
+                cv2.line(frame, topRight, bottomRight, (0, 255, 0), 2)
+                cv2.line(frame, bottomRight, bottomLeft, (0, 255, 0), 2)
+                cv2.line(frame, bottomLeft, topLeft, (0, 255, 0), 2)
+
+                # compute and draw the center (x, y)-coordinates of the
+                # ArUco marker
+                cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+                cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+                cv2.circle(frame, (cX, cY), 4, (0, 0, 255), -1)
+
+                # draw the ArUco marker ID on the frame
+                cv2.putText(frame, str(markerID),
+                            (topLeft[0], topLeft[1] - 15),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        cv2.imshow("camera", frame)
         cv2.waitKey(1)
 
 
