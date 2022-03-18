b38a02836c9d8a2edaaa6403cf4af43f0376e587
Filip Parag <filip@parag.rs>
2022-03-18 16:33:13 +0100

    bc dependency

--- docker/provision/mep3.mk
+++ docker/provision/mep3.mk
@@ -13,6 +13,7 @@ mep3-rosdep:
 	cd /memristor/ros2_ws && yes | rosdep --rosdistro ${ROS_DISTRO} install --from-paths src --ignore-src
 
 mep3-bashrc:
+	sudo -E apt-get install -y bc
 	echo 'source /opt/ros/${ROS_DISTRO}/local_setup.bash' >> /memristor/.bashrc
 	ln -sf /memristor/ros2_ws/src/mep3/docker/config/shortcuts.sh /memristor/.setup/config/shortcuts.sh
 	echo 'cd /memristor/ros2_ws' >> /memristor/.bashrc
