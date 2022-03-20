b0b3f151c777eae5af18baa65524f89d8239db2d
Filip Parag <filip@parag.rs>
2022-03-20 15:17:13 +0100

    Terminal shortcuts (#126)

--- docker/provision/mep3.mk
+++ docker/provision/mep3.mk
@@ -13,5 +13,12 @@ mep3-rosdep:
 	cd /memristor/ros2_ws && yes | rosdep --rosdistro ${ROS_DISTRO} install --from-paths src --ignore-src
 
 mep3-bashrc:
+	sudo -E apt-get install -y bc
 	echo 'source /opt/ros/${ROS_DISTRO}/local_setup.bash' >> /memristor/.bashrc
+	ln -sf /memristor/ros2_ws/src/mep3/docker/config/shortcuts.sh /memristor/.setup/config/shortcuts.sh
 	echo 'cd /memristor/ros2_ws' >> /memristor/.bashrc
+
+mep3-symlink-setup:
+	ln -sf /memristor/ros2_ws/src/mep3/docker/Makefile /memristor/.setup/Makefile
+	rm -rf /memristor/.setup/provision
+	ln -sf /memristor/ros2_ws/src/mep3/docker/provision /memristor/.setup/provision
