abf8c400584b814e8880933dbea1055edc921b64
Vladimir Vincan <vladimirvincan@gmail.com>
2022-04-08 02:40:27 +0200

    driver doesn't work

--- mep3_simulation/mep3_simulation/webots_binary_ranger_driver.py
+++ mep3_simulation/mep3_simulation/webots_binary_ranger_driver.py
@@ -1,23 +1,21 @@
 from math import pi, inf
-from ranger_msgs.msg import Range
 import rclpy
-from rclpy.action import ActionClient
-from rclpy.callback_groups import ReentrantCallbackGroup
 from rclpy.executors import MultiThreadedExecutor
-
+from rclpy.node import Node
+from sensor_msgs.msg import Range
 
 class WebotsBinaryRangerDriver:
     """
-    Binary ranger returns threshold value if object is detected else inf.
+    Binary ranger returns -inf if object is detected else inf.
 
-    On our simulated robot, the distance rangers return the true distance from
+    On our simulated robot, the distance sensors return the true distance from
     objects, whereas in the driver we convert them in binary rangers, in order
     to be compliant with the physical system.
-    In Webots we have four distance rangers:
-    - distance_ranger_front_left
-    - distance_ranger_front_right
-    - distance_ranger_rear_left
-    - distance_ranger_rear_right
+    In Webots we have four distance sensors:
+    - distance_sensor_front_left
+    - distance_sensor_front_right
+    - distance_sensor_rear_left
+    - distance_sensor_rear_right
     In ros2, we create four topics:
     - binary_ranger_command/front_left
     - binary_ranger_command/front_right
@@ -31,41 +29,45 @@ class WebotsBinaryRangerDriver:
         except Exception:  # noqa: E501
             pass  # noqa: E501
         self.__executor = MultiThreadedExecutor()
-
-        self.__robot = webots_node.robot
+        
         namespace = properties['namespace']
         ranger_position = properties['binaryRangerPosition']
-        self.__distance_ranger = self.__robot.getDevice(
-            f'distance_ranger_{ranger_position}')
+
         self.__node = rclpy.create_node(
             f'webots_binary_ranger_{ranger_position}')
+        self.__robot = webots_node.robot
+        self.__distance_sensor = self.__robot.getDevice(
+            f'distance_sensor_{ranger_position}')
         timestep = int(self.__robot.getBasicTimeStep())
-        self.__distance_ranger.enable(timestep)
+        self.__distance_sensor.enable(timestep)
+        
         self.__DISTANCE_THRESHOLD_M = 0.3
         self.__FOW_RAD = pi / 6
-        self.__ranger_action = ActionClient(
-            self.__node, Range,
-            f'{namespace}/binary_ranger_command/{ranger_position}')
-
+        timer_period = 0.5  # seconds
+            
+        self.__publisher = self.create_publisher(Range, f'{namespace}/binary_ranger_command/{ranger_position}', 10)
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+            
     def send_goal(self, range):
-        # This is a fixed distance ranger, we could also write
+        # This is a fixed distance ranger, we can write
         # min_range===max_range===distance
 
         goal_msg = Range.Goal()
         goal_msg.ratiation_type = 1  # ULTRASOUND=0 | INFRARED=1
         goal_msg.field_of_view = self.__FOW_RAD  # radians
         goal_msg.min_range = self.__DISTANCE_THRESHOLD_M  # meters
-        goal_msg.max_range = inf
+        goal_msg.max_range = self.__DISTANCE_THRESHOLD_M  # meters
         goal_msg.range = range  # meters
 
-        self._action_client.wait_for_server()
-        return self._action_client.send_goal_async(goal_msg)
-
-    def step(self):
-        distance = self.__distance_ranger.getValue()
+        return self.__publisher.publish(goal_msg)
+        
+    def timer_callback(self):
+        distance = self.__distance_sensor.getValue()
         distance = distance <= self.__DISTANCE_TRESHOLD_M
         if distance:
-            self.send_goal(self.__DISTANCE_THRESHOLD_M)
+            self.send_goal(-inf)
         else:
             self.send_goal(inf)
+
+    def step(self):
         rclpy.spin_once(self.__node, timeout_sec=0, executor=self.__executor)
