8502b265f48586f1316b94fac809473a29c579c9
Dusan Jovanovic <angstrem98@gmail.com>
2022-04-08 19:21:36 +0000

    fix

--- mep3_driver/mep3_driver/vacuum_pump_driver.py
+++ mep3_driver/mep3_driver/vacuum_pump_driver.py
@@ -1,60 +1,42 @@
 import struct
 import can
-
-from distutils import command
 from functools import partial
-from traceback import print_tb
-
 from rclpy.action import ActionServer, CancelResponse, GoalResponse
 from rclpy.callback_groups import ReentrantCallbackGroup
 from rclpy.executors import MultiThreadedExecutor
-
 from threading import Lock, current_thread
-
 from mep3_msgs.action import VacuumPumpCommand
-
-VACUUM_PUMP_CAN_ID = 0x00006C10
-
-VACUUMPUMPS = [
-    {'id': 4, 'name': 'pump_left_hand'},
-    {'id': 5, 'name': 'pump_right_hand'},
-    ]
 import rclpy
 from rclpy.node import Node
 
-class VacuumPump:
-    def __init__(self, pump_id, pump_name):
-        
-        self.id = pump_id
-        self.name = pump_name
 
-
-
+VACUUM_PUMP_CAN_ID = 0x00006C10
+VACUUMPUMPS = [
+    {'id': 0, 'name': 'pump_left_hand'},
+    {'id': 1, 'name': 'pump_right_hand'},
+]
 
 
 class VacuumPumpDriver(Node):
-    def __init__(self, pump, can_mutex, can_bus):
-        super().__init__('Vacuum_pump' + str(pump['id']))
+    def __init__(self, can_mutex, can_bus):
+        super().__init__('vacuum_pump_driver')
         self.__actions = []
         self.pump_list = []
         self.bus = can_bus
-
-        new_pump = VacuumPump(
-            pump_id=pump['id'], pump_name=pump['name']
-        )
-        self.pump_list.append(new_pump)
-
-        self.__actions.append(
-            ActionServer(
-                self,
-                VacuumPumpCommand,
-                f'vacuum_command/{pump["name"]}',
-                execute_callback=partial(self.__handle_vacuum_pump_command, vacuum_pump=new_pump),
-                callback_group=ReentrantCallbackGroup(),
-                goal_callback=self.__goal_callback,
-                cancel_callback=self.__cancel_callback
+        
+        callback_group = ReentrantCallbackGroup()
+        for pump in VACUUMPUMPS:
+            self.__actions.append(
+                ActionServer(
+                    self,
+                    VacuumPumpCommand,
+                    f'vacuum_command/{pump["name"]}',
+                    execute_callback=partial(self.__handle_vacuum_pump_command, pump_id=pump['id']),
+                    callback_group=callback_group,
+                    goal_callback=self.__goal_callback,
+                    cancel_callback=self.__cancel_callback
+                )
             )
-        )
 
     def __goal_callback(self, _):
         return GoalResponse.ACCEPT
@@ -62,30 +44,27 @@ class VacuumPumpDriver(Node):
     def __cancel_callback(self, _):
         return CancelResponse.ACCEPT
 
-    def __handle_vacuum_pump_command(self, goal_handle,
-                                   vacuum_pump: VacuumPump = None):
+    def __handle_vacuum_pump_command(self, goal_handle, pump_id=None):
         self.get_logger().info(str(goal_handle.request))
-        self.get_logger().info("Vacuum pump ID: " + str(vacuum_pump.id))
+        self.get_logger().info("Vacuum pump ID: " + str(pump_id))
         self.get_logger().info("Thread: " + str(current_thread().name))
 
         connect = goal_handle.request.connect
 
         result = VacuumPumpCommand.Result()
 
-        pump_id = VACUUM_PUMP_CAN_ID + vacuum_pump.id
-        
+        pump_id = VACUUM_PUMP_CAN_ID + pump_id
 
         bin_data = struct.pack('b', connect)
 
-        msg = can.Message(arbitration_id=pump_id, 
-                         data=bin_data,
-                         is_extended_id=True)
+        msg = can.Message(arbitration_id=pump_id,
+                          data=bin_data,
+                          is_extended_id=True)
 
         try:
             self.bus.send(msg)
         except can.CanError:
             self.get_logger().info("CAN ERROR: Cannot send message over CAN bus. Check if can0 is active.")
-
 
         result.result = connect  # TODO: check sensor board?
         goal_handle.succeed()
@@ -101,11 +80,11 @@ def main(args=None):
     # Set filters for receiving data
     bus.set_filters(filters=[{"can_id": VACUUM_PUMP_CAN_ID, "can_mask": 0x1FFFFFFF, "extended": True}])
     executor = MultiThreadedExecutor()
-    for vacuum_pump in VACUUMPUMPS:
-        driver = VacuumPumpDriver(vacuum_pump, can_mutex, bus)
-        executor.add_node(driver)
+    driver = VacuumPumpDriver(can_mutex, bus)
+    executor.add_node(driver)
     executor.spin()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
