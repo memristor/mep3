8554b910c2709223e722a70f4c56e3386bb744ee
angstrem98 <angstrem98@gmail.com>
2022-04-11 18:29:43 +0200

    lint + uncustify

--- mep3_navigation/src/distance_angle/distance_angle_regulator.cpp
+++ mep3_navigation/src/distance_angle/distance_angle_regulator.cpp
@@ -521,7 +521,8 @@ void DistanceAngleRegulator::navigate_to_pose()
   // Don't execute movement if in tolerance
   if (
     distance_to_goal <= distance_goal_tolerance_ &&
-    std::abs(angle_normalize(goal_angle - map_robot_angle_)) <= angle_goal_tolerance_) {
+    std::abs(angle_normalize(goal_angle - map_robot_angle_)) <= angle_goal_tolerance_)
+  {
     action_running_ = false;
     navigate_to_pose_server_->succeeded_current(result);
     return;
@@ -573,18 +574,20 @@ void DistanceAngleRegulator::navigate_to_pose()
         break;
 
       case MotionState::MOVING_TO_GOAL:
-        RUN_EACH_NTH_CYCLES(uint8_t, 4, {
-          delta_x = goal_x - map_robot_x_;
-          delta_y = goal_y - map_robot_y_;
-          distance_to_goal = std::hypot(delta_x, delta_y);
-          angle_to_goal = std::atan2(delta_y, delta_x);
-          // refresh only for longer moves
-          if (distance_to_goal > 0.1) {
-            // refresh both distance and angle
-            motion_profile_input_.target_position[0] = odom_robot_distance_ + direction * distance_to_goal;
-            rotate_absolute(angle_to_goal + (direction < 0 ? M_PI : 0.0));
-          }
-        })
+        RUN_EACH_NTH_CYCLES(
+          uint8_t, 4, {
+        delta_x = goal_x - map_robot_x_;
+        delta_y = goal_y - map_robot_y_;
+        distance_to_goal = std::hypot(delta_x, delta_y);
+        angle_to_goal = std::atan2(delta_y, delta_x);
+        // refresh only for longer moves
+        if (distance_to_goal > 0.1) {
+          // refresh both distance and angle
+          motion_profile_input_.target_position[0] =
+          odom_robot_distance_ + direction * distance_to_goal;
+          rotate_absolute(angle_to_goal + (direction < 0 ? M_PI : 0.0));
+        }
+      })
 
         if (motion_profile_finished()) {
           timeout_counter--;
