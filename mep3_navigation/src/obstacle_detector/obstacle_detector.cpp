7e5b72ab40782f14d5e8b0ef3893ddc55bd18880
Vladimir Vincan <vladimirvincan@gmail.com>
2022-04-15 12:27:48 +0200

    clang-format

--- mep3_navigation/src/obstacle_detector/obstacle_detector.cpp
+++ mep3_navigation/src/obstacle_detector/obstacle_detector.cpp
@@ -15,8 +15,8 @@
 #include <cmath>
 #include <limits>
 #include <memory>
-#include <vector>
 #include <string>
+#include <vector>
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
@@ -29,26 +29,35 @@
 
 using std::placeholders::_1;
 
-class ObstacleDetector : public rclcpp::Node
-{
-public:
+class ObstacleDetector : public rclcpp::Node {
+ public:
   ObstacleDetector() : Node("obstacle_detector") {
     tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
-    transform_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
-    laser_scan_subscriber_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
-      "scan_inflated", 10, std::bind(&ObstacleDetector::laser_scan_callback, this, _1));
+    transform_listener_ =
+        std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+    laser_scan_subscriber_ =
+        this->create_subscription<sensor_msgs::msg::LaserScan>(
+            "scan_inflated", 10,
+            std::bind(&ObstacleDetector::laser_scan_callback, this, _1));
     range_fl_subscriber_ = this->create_subscription<sensor_msgs::msg::Range>(
-                                                                              "binary_ranger_command/front_left", 10, std::bind(&ObstacleDetector::range_callback_fl, this, _1));
+        "binary_ranger_command/front_left", 10,
+        std::bind(&ObstacleDetector::range_callback_fl, this, _1));
     range_fr_subscriber_ = this->create_subscription<sensor_msgs::msg::Range>(
-                                                                              "binary_ranger_command/front_right", 10, std::bind(&ObstacleDetector::range_callback_fr, this, _1));
+        "binary_ranger_command/front_right", 10,
+        std::bind(&ObstacleDetector::range_callback_fr, this, _1));
     range_rl_subscriber_ = this->create_subscription<sensor_msgs::msg::Range>(
-                                                                              "binary_ranger_command/rear_left", 10, std::bind(&ObstacleDetector::range_callback_rl, this, _1));
+        "binary_ranger_command/rear_left", 10,
+        std::bind(&ObstacleDetector::range_callback_rl, this, _1));
     range_rr_subscriber_ = this->create_subscription<sensor_msgs::msg::Range>(
-                                                                              "binary_ranger_command/rear_right", 10, std::bind(&ObstacleDetector::range_callback_rr, this, _1));
-    publisher_ = this->create_publisher<std_msgs::msg::Bool>("obstacle_detected", 10);
+        "binary_ranger_command/rear_right", 10,
+        std::bind(&ObstacleDetector::range_callback_rr, this, _1));
+    publisher_ =
+        this->create_publisher<std_msgs::msg::Bool>("obstacle_detected", 10);
   }
-private:
-  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_scan_subscriber_;
+
+ private:
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr
+      laser_scan_subscriber_;
   rclcpp::Subscription<sensor_msgs::msg::Range>::SharedPtr range_fl_subscriber_;
   rclcpp::Subscription<sensor_msgs::msg::Range>::SharedPtr range_fr_subscriber_;
   rclcpp::Subscription<sensor_msgs::msg::Range>::SharedPtr range_rl_subscriber_;
@@ -61,47 +70,41 @@ private:
   bool detection_rl = false;
   bool detection_rr = false;
 
-  void laser_scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
-  {
+  void laser_scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg) {
     sensor_msgs::msg::LaserScan scan = *msg;
   }
 
-  void range_callback_fl(const sensor_msgs::msg::Range::SharedPtr msg)
-  {
+  void range_callback_fl(const sensor_msgs::msg::Range::SharedPtr msg) {
     sensor_msgs::msg::Range scan = *msg;
     detection_fl = scan.range > 0;
     publish();
   }
 
-  void range_callback_fr(const sensor_msgs::msg::Range::SharedPtr msg)
-  {
+  void range_callback_fr(const sensor_msgs::msg::Range::SharedPtr msg) {
     sensor_msgs::msg::Range scan = *msg;
     detection_fr = scan.range > 0;
     publish();
   }
 
-  void range_callback_rl(const sensor_msgs::msg::Range::SharedPtr msg)
-  {
+  void range_callback_rl(const sensor_msgs::msg::Range::SharedPtr msg) {
     sensor_msgs::msg::Range scan = *msg;
     detection_rl = scan.range > 0;
     publish();
   }
 
-  void range_callback_rr(const sensor_msgs::msg::Range::SharedPtr msg)
-  {
+  void range_callback_rr(const sensor_msgs::msg::Range::SharedPtr msg) {
     sensor_msgs::msg::Range scan = *msg;
     detection_rr = scan.range > 0;
     publish();
   }
 
-  void publish()
+  void publish(){
+    publisher_->publish(detection_fl | detection_fr | detection_rl | detection_rr);
+  }
 };
 
 
-publisher_->publish(detection_fl | detection_fr | detection_rl | detection_rr);
-
-int main(int argc, char * argv[])
-{
+int main(int argc, char* argv[]) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<ObstacleDetector>());
   rclcpp::shutdown();
