fc74b2373c5c98774582f4f6c0decd261561a02e
Dusan Jovanovic <angstrem98@gmail.com>
2022-04-20 16:41:35 +0000

    push somebody's work

--- mep3_driver/mep3_driver/resistance_meter_driver.py
+++ mep3_driver/mep3_driver/resistance_meter_driver.py
@@ -79,7 +79,9 @@ class ResistanceMeterDriver(Node):
         print(recv)
         if recv is not None:
             readout = struct.unpack('<H', recv.data[:2])[0]
+            self.get_logger().info("->>>>>>>>VALUE FROM CAN: " + str(readout))
             resistance = readout_to_resistance(readout)
+            self.get_logger().info("Resistance: " + str(resistance))
 
         self.can_mutex.release()
 
