939e532374d64c4309f1722a2c1f0f08041d0272
Filip Parag <filip@parag.rs>
2022-05-18 21:34:01 +0200

    remove bloat

--- mep3_behavior_tree/include/mep3_behavior_tree/table_specific_ports.hpp
+++ mep3_behavior_tree/include/mep3_behavior_tree/table_specific_ports.hpp
@@ -1,27 +1,3 @@
-#ifndef GOAL_TABLES
-#define GOAL_TABLES \
-    BT::InputPort<std::string>("goal_foo"), \
-    BT::InputPort<std::string>("goal_bar")
-#endif
-
-#ifndef POSITION_TABLES
-#define POSITION_TABLES \
-    BT::InputPort<std::string>("position_foo"), \
-    BT::InputPort<std::string>("position_bar")
-#endif
-
-#ifndef RESISTANCE_TABLES
-#define RESISTANCE_TABLES \
-    BT::InputPort<std::string>("resistance_foo"), \
-    BT::InputPort<std::string>("resistance_bar")
-#endif
-
-#ifndef VALUE_TABLES
-#define VALUE_TABLES \
-    BT::InputPort<std::string>("value_foo"), \
-    BT::InputPort<std::string>("value_bar")
-#endif
-
 // Copyright 2022 Memristor Robotics
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
