6b4e0dab5f525f7d20258c9a41d8ec7776dea1b2
Filip Parag <filip@parag.rs>
2022-05-19 22:46:09 +0200

    input validation

--- mep3_behavior_tree/include/mep3_behavior_tree/blackboard_control_flow.hpp
+++ mep3_behavior_tree/include/mep3_behavior_tree/blackboard_control_flow.hpp
@@ -35,6 +35,57 @@ public:
   ) : BT::ControlNode(name, config_)
   {
     this->running_child_ = -1;
+
+    // Check number of branches
+    if (this->childrenCount() > 2) {
+      throw BT::RuntimeError(
+        "Too many children nodes for CompareBlackboard"
+      );
+    }
+    if (!getInput("key", this->key) || this->key == "") {
+      throw BT::RuntimeError(
+        "Blackboard key can't be empty"
+      );
+    }
+
+    // Default port values
+    if (!getInput("operator", this->op)) {
+      this->op = "eq";
+    }
+    if (!getInput("type", this->type)) {
+      this->type = "str";
+    }
+    if (!getInput("value", this->value)) {
+      if (this->type == "str")
+        this->value = "";
+      else
+        this->value = "0";
+    }
+
+    // Input validation
+    if (this->type == "str" && this->op != "eq") {
+      throw BT::RuntimeError(
+        "Type 'str' can only be compared using 'eq' operator"
+      );
+    }
+    if (
+      this->type != "str" && \
+      this->type != "i64" && \
+      this->type != "f64"
+    ) {
+      throw BT::RuntimeError(
+        "Unknown type + '" + type + "'"
+      );
+    }
+    if (
+      this->op != "eq" && \
+      this->op != "lt" && this->op != "le" && \
+      this->op != "gt" && this->op != "ge"
+    ) {
+      throw BT::RuntimeError(
+        "Unknown operator + '" + op + "'"
+      );
+    }
   }
 
   CompareBlackboard() = delete;
@@ -42,8 +93,13 @@ public:
   static BT::PortsList providedPorts()
   {
     return {
+      // Supported value types for operators:
+      //  str    std::string    eq       (default)
+      //  i64    int64_t        eq, lt, le, gt, ge
+      //  i64    _Float64       eq, lt, le, gt, ge
       BT::InputPort<std::string>("operator"),
-      BT::InputPort<std::string>("output_key"),
+      BT::InputPort<std::string>("type"),
+      BT::InputPort<std::string>("key"),
       BT::InputPort<std::string>("value")
     };
   }
@@ -64,6 +120,7 @@ public:
   }
 private:
     ssize_t running_child_;
+  std::string op, type, key, value;
 };
 
 } // namespace mep3_behavior_tree
