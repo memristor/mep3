b689798722b22f3c4cba37b843ee95100ea3bf01
MarijaGolubovic <93673097+MarijaGolubovic@users.noreply.github.com>
2022-11-04 16:30:21 +0100

    Playing area for  2023 (#227)
    * Bug fix - delete the duplicate function
    
    * Adapt the playing area to a new theme
    
    * fixed positions and orientations of walls, fixed beacon supports, central tracking device and basket supports
    
    * finished first version of terrain
    
    * set viewpoint. reposition opponent boxes
    
    * fix cherry support name warning
    
    * added color plate to top of FixedBeaconSupport.proto
    
    * Remove Statuette and Replica drivers from config urdf files and judge.py
    
    * fix initial positions of opponent robots in judge.py
    
    * fix Webots shortcut
    
    * fix Webots shortcut
    
    * remove mep3_localization
    
    * fix behavior_tree_cpp_v3 package name in CMakeLists.txt. Change simulation launch file to eurobot_2023.
    
    * update all PROTO files to R2022b Webots version
    
    Co-authored-by: VladimirVincan <vladimirvincan@gmail.com>

--- mep3_simulation/webots_data/controllers/judge/judge.py
+++ mep3_simulation/webots_data/controllers/judge/judge.py
@@ -43,31 +43,21 @@ def main():
     robot_opponent_small = ObjectManipulator(supervisor,
                                              'ROBOT_OPPONENT_SMALL')
 
-    # Statuette and replica
-    statuette = ObjectManipulator(supervisor, 'STATUETTE')
-    replica = ObjectManipulator(supervisor, 'REPLICA')
-
     # Set initial poses
     if color == 'yellow':
         robot_big.set_position(x=-1.2491, y=0.12, theta=-pi / 2)
-        robot_small.set_position(x=-1.249, y=0.47, theta=pi/2)
-        robot_opponent_big.set_position(x=1.26, y=0.46, theta=pi)
-        robot_opponent_small.set_position(x=1.26, y=0.128, theta=pi)
+        robot_small.set_position(x=-1.249, y=0.47, theta=pi / 2)
+        robot_opponent_big.set_position(x=0.775, y=0.375, theta=pi)
+        robot_opponent_small.set_position(x=0.775, y=-1.275, theta=pi)
 
         supervisor.step(timestep)
-
-        statuette.set_position(x=-1.267, y=-0.768, z=0.125, theta=0.785)
-        replica.set_position(x=-1.2621, y=0.17, z=0.1579, theta=pi)
     else:
         robot_big.set_position(x=1.249, y=0.47, theta=pi / 2)
-        robot_small.set_position(x=1.2491, y=0.12, theta=-pi/2)
-        robot_opponent_big.set_position(x=-1.26, y=0.46, theta=0)
-        robot_opponent_small.set_position(x=-1.26, y=0.128, theta=0)
+        robot_small.set_position(x=1.2491, y=0.12, theta=-pi / 2)
+        robot_opponent_big.set_position(x=-0.775, y=0.375, theta=0)
+        robot_opponent_small.set_position(x=-0.775, y=-1.275, theta=0)
 
         supervisor.step(timestep)
-
-        statuette.set_position(x=1.267, y=-0.768, z=0.125, theta=2.356)
-        replica.set_position(x=1.2491, y=0.1521, z=0.1579, theta=pi / 2)
 
     # Do something
     while supervisor.step(timestep) != -1:
