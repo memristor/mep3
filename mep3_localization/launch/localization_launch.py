b97bf58364c5a96bf524626049838bd271a98f6d
VladimirVincan <VladimirVincan@gmail.com>
2022-11-23 11:08:18 +0100

    ArUco Robot Localization (#32)
    * added raspicam
    
    * init
    
    * nav2 typo fix
    
    * fix nav2
    
    * simulation_launch update for camera_central
    
    * video frame received
    
    * added obj files. centered all objects around hinges
    
    * pose estimation
    
    * publish to tf
    
    * new obj files
    
    * finished robot
    
    * added replica connector, added robot to eurobot map
    
    * fix viewpoint
    
    * add extern controller
    
    * working strategy
    
    * fix aruco size
    
    * return WaitMatchStartAction
    
    * bounding object fix
    
    * fix metalness
    
    * fix metalness
    
    * merge robot_update
    
    * change cam position. change aruco dimensions in .py file
    
    * orientation is correct
    
    * end of day commit
    
    * Apply PR suggestion
    
    Co-authored-by: Darko Lukić <lukicdarkoo@gmail.com>
    
    * Apply PR suggestion
    
    * linter fix
    
    * linter fix
    
    * linter copyright fix
    
    * remove .bak
    
    * solved z axis flipping
    
    * camera info listener
    
    * camera info listener
    
    * autopep8
    
    * autopep8
    
    * trying to fix static_tf_listener
    
    * works. added launch files
    
    * remove static broadcaster
    
    * linter fix
    
    * remove calibration matrix yaml
    
    * remove build in cam controller
    
    * remove bak file
    
    * revert world file
    
    * remove Pogon.proto
    
    * remove RobotMemristor.proto
    
    * remove empty files
    
    * revert empty file
    
    * change webots robot env name
    
    * deleted camera controller
    
    * make static cvbridge
    
    * Bug fix - delete the duplicate function
    
    * Adapt the playing area to a new theme
    
    * fixed positions and orientations of walls, fixed beacon supports, central tracking device and basket supports
    
    * finished first version of terrain
    
    * set viewpoint. reposition opponent boxes
    
    * fix cherry support name warning
    
    * added color plate to top of FixedBeaconSupport.proto
    
    * Remove Statuette and Replica drivers from config urdf files and judge.py
    
    * fix initial positions of opponent robots in judge.py
    
    * fix Webots shortcut
    
    * fix Webots shortcut
    
    * remove mep3_localization
    
    * fix behavior_tree_cpp_v3 package name in CMake lists
    
    * Change WEBOTS_ROBOT_NAME to WEBOTS_CONTROLLER_URL. Add Ros2SupervisorLauncher.
    
    * finished aruco_detector.py
    
    * Created pose_corrector.py
    
    * swap child and parent in tf2 for raw_camera
    
    * added send_raw_table_markers().
    
    * end of day commit
    
    * Deleted PoseCorrector, RobotDetection and ObjectLocalizer. Added everything to ArucoDetector.
    
    * apply Darko's comments
    
    * fix
    
    * cleanup
    
    * Fix error if marker_[20] is not found.
    
    * Working on debug and use_localization options. Not finished.
    
    * Add use_localization and debug launch configurations. Fix Viewpoint.
    
    * Change name 'cam' to 'camera' everywhere.
    
    * Fix color of robots. purple->green and yellow->blue.
    
    * Fix check_alignment().
    Update description.
    Check_alignment() now as input has a transformation matrix.
    Fix static_tf_listener() - [q.w, q.x, q.y, q.z] and parent <- child order in lookup_transform().
    
    * Add debug mode to aruco_detector.py
    
    * Make all functions and variables private.
    
    * Change transformation_matrix to tmat.
    
    * delete flycheck
    
    * Remove word listen from callback function names.
    
    * Delete tf_base_link_laser from simulation_launch.py
    
    * Static__tf_listener() -> __static_tf_callback() in description
    
    Co-authored-by: Darko Lukić <lukicdarkoo@gmail.com>
    Co-authored-by: Marija <marijagolubovic16@gmail.com>

---
+++ mep3_localization/launch/localization_launch.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+# Copyright 2022 Memristor Robotics
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, PythonExpression
+
+
+def generate_launch_description():
+    """
+    It is necessary to have a prediction of the camera position
+    in order to detect incorrect orientations of ArUco markers.
+
+    Debug launch parameter is used to publish static marker positions.
+    """
+    debug = LaunchConfiguration('debug', default=False)
+
+    return LaunchDescription([
+        Node(package='tf2_ros',
+             executable='static_transform_publisher',
+             arguments=[
+                 '-0.1', '1.50976', '1.05','-4.85921e-06', '-0.965927', '0.258816', '1.32679e-06', 'map', 'camera_prediction'
+             ],
+             condition = IfCondition(debug)),
+        Node(package='tf2_ros',
+             executable='static_transform_publisher',
+             arguments=[
+                 '-0.430', '0.925', '0', '0', '0', '0', '1', 'map', 'marker_[20]_static'
+             ],
+             condition = IfCondition(debug)),
+        Node(package='tf2_ros',
+             executable='static_transform_publisher',
+             arguments=[
+                 '0.430', '0.925', '0', '0', '0', '0', '1', 'map', 'marker_[21]_static',
+             ],
+             condition = IfCondition(debug)),
+        Node(package='tf2_ros',
+             executable='static_transform_publisher',
+             arguments=[
+                 '-0.430', '-0.925', '0', '0', '0', '0', '1', 'map', 'marker_[22]_static',
+             ],
+             condition = IfCondition(debug)),
+        Node(package='tf2_ros',
+             executable='static_transform_publisher',
+             arguments=[
+                 '0.430', '-0.925', '0', '0', '0', '0', '1', 'map', 'marker_[23]_static',
+             ],
+             condition = IfCondition(debug)),
+        Node(package='mep3_localization', executable='aruco_detector',
+             output='screen',  # debugging
+             emulate_tty=True,  # debugging
+             parameters=[
+                 {'debug': debug}
+             ]
+             ),
+    ])
