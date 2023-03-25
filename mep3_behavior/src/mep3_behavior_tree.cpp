4fb4fac92586a062e037a13e08647dea3addb2e9
Darko Lukic <lukicdarkoo@gmail.com>
2023-03-25 11:19:21 +0100

    Execute strategies on save

--- mep3_behavior/src/mep3_behavior_tree.cpp
+++ mep3_behavior/src/mep3_behavior_tree.cpp
@@ -16,6 +16,9 @@
 #define ASSETS_DIRECTORY "mep3_behavior/strategies"
 #endif
 
+#include <sys/types.h>
+#include <sys/stat.h>
+
 #include <filesystem>
 #include <iostream>
 #include <set>
@@ -44,63 +47,26 @@
 
 using KeyValueT = diagnostic_msgs::msg::KeyValue;
 
-int main(int argc, char **argv)
+time_t get_last_modification_time()
 {
-  // Initialize ROS node
-  rclcpp::init(argc, argv);
-  auto node = rclcpp::Node::make_shared("mep3_behavior");
-
-  // Initialize blackboard
-
-  auto blackboard = BT::SharedBlackboard::create(node);
-
-  // Create shared blackboard topic
-  auto blackboard_subscription = node->create_subscription<KeyValueT>(
-      "/shared_blackboard",
-      rclcpp::SystemDefaultsQoS().reliable().transient_local(),
-      [blackboard](const KeyValueT::SharedPtr msg)
-      {
-        blackboard->set(msg->key, msg->value);
-      });
-
-  // Set namespace
-  std::string name(node->get_namespace());
-  name = name.replace(name.find("/"), sizeof("/") - 1, "");
-  blackboard->set("namespace", name);
-
-  // Get strategy name
-  node->declare_parameter<std::string>("strategy", "strategy");
-  auto strategy = node->get_parameter("strategy").as_string();
-
-  auto tree_file_path = (std::filesystem::path(ASSETS_DIRECTORY) / strategy).replace_extension(".xml");
-  if (!std::filesystem::exists(tree_file_path))
+  time_t max_time = 0;
+  for (auto const &entry : std::filesystem::directory_iterator(ASSETS_DIRECTORY))
   {
-    std::cerr << "Error: Strategy file '" << strategy
-              << "' does not exist" << std::endl;
-    return 1;
+    if (entry.path().extension() == ".xml")
+    {
+      struct stat result;
+      if (stat(entry.path().string().c_str(), &result) == 0)
+      {
+        if (result.st_mtime > max_time)
+          max_time = result.st_mtime;
+      }
+    }
   }
+  return max_time;
+}
 
-  // Set table
-  node->declare_parameter<std::string>("table", "");
-  auto table = node->get_parameter("table");
-  blackboard->set("table", table.as_string());
-
-  // Get predefined table names
-  node->declare_parameter<std::vector<std::string>>("predefined_tables", std::vector<std::string>({}));
-  rclcpp::Parameter predefined_tables(
-      "predefined_tables",
-      std::vector<std::string>({}));
-  node->get_parameter("predefined_tables", predefined_tables);
-  blackboard->set("predefined_tables", predefined_tables.as_string_array());
-
-  // Set color
-  node->declare_parameter<std::string>("color", "blue");
-  auto color = node->get_parameter("color");
-  if (color.as_string() == "green")
-    blackboard->set("color", BT::TeamColor::GREEN);
-  else
-    blackboard->set("color", BT::TeamColor::BLUE);
-
+void start_behavior_tree(rclcpp::Node::SharedPtr node, std::string strategy, BT::Blackboard::Ptr blackboard, time_t last_modification_time, bool should_live_reload)
+{
   BT::BehaviorTreeFactory factory;
   factory.registerNodeType<mep3_behavior::CanbusSendAction>(
       "CanbusSend");
@@ -139,6 +105,78 @@ int main(int argc, char **argv)
   {
     finish = tree.tickOnce() == BT::NodeStatus::SUCCESS;
     tree.sleep(std::chrono::milliseconds(10));
+    if (should_live_reload && get_last_modification_time() > last_modification_time)
+    {
+      tree.haltTree();
+      return;
+    }
+  }
+}
+
+int main(int argc, char **argv)
+{
+  // Initialize ROS node
+  rclcpp::init(argc, argv);
+  auto node = rclcpp::Node::make_shared("mep3_behavior");
+
+  // Initialize blackboard
+  auto blackboard = BT::SharedBlackboard::create(node);
+
+  // Create shared blackboard topic
+  auto blackboard_subscription = node->create_subscription<KeyValueT>(
+      "/shared_blackboard",
+      rclcpp::SystemDefaultsQoS().reliable().transient_local(),
+      [blackboard](const KeyValueT::SharedPtr msg)
+      {
+        blackboard->set(msg->key, msg->value);
+      });
+
+  // Get parameters
+  std::string name(node->get_namespace());
+  name = name.replace(name.find("/"), sizeof("/") - 1, "");
+  node->declare_parameter<std::string>("strategy", "strategy");
+  auto strategy = node->get_parameter("strategy").as_string();
+  node->declare_parameter<std::string>("table", "");
+  auto table = node->get_parameter("table");
+  node->declare_parameter<std::vector<std::string>>("predefined_tables", std::vector<std::string>({}));
+  rclcpp::Parameter predefined_tables(
+      "predefined_tables",
+      std::vector<std::string>({}));
+  node->get_parameter("predefined_tables", predefined_tables);
+  node->declare_parameter<std::string>("color", "blue");
+  BT::TeamColor color = node->get_parameter("color").as_string() == "green" ? BT::TeamColor::GREEN : BT::TeamColor::BLUE;
+
+  // Live reloading
+  bool should_live_reload = (strategy.find("live") != std::string::npos);
+  if (should_live_reload)
+  {
+    RCLCPP_WARN(node->get_logger(), "Live reloading is enabled!");
+  }
+
+  // Populate blackboard
+  blackboard->set("namespace", name);
+  blackboard->set("predefined_tables", predefined_tables.as_string_array());
+  blackboard->set("color", color);
+  blackboard->set("table", table.as_string());
+
+  while (rclcpp::ok())
+  {
+    start_behavior_tree(node, strategy, blackboard, get_last_modification_time(), should_live_reload);
+    if (should_live_reload)
+    {
+      RCLCPP_WARN(node->get_logger(), "Reloading tree!");
+
+      blackboard->clear();
+      blackboard->set("namespace", name);
+      blackboard->set("predefined_tables", predefined_tables.as_string_array());
+      blackboard->set("color", color);
+      blackboard->set("table", table.as_string());
+      blackboard->set("node", node);
+    }
+    else
+    {
+      break;
+    }
   }
   rclcpp::shutdown();
   return 0;
