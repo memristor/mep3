60f8c0ffbf22ee0e38f1d0b9d1bfd6984e629326
VladimirVincan <vladimirvincan@gmail.com>
2023-04-03 23:22:58 +0200

    added PLUGINLIB_EXPORT_CLASS

--- mep3_navigation/src/stuck_behavior/stuck_behavior.cpp
+++ mep3_navigation/src/stuck_behavior/stuck_behavior.cpp
@@ -1,9 +1,11 @@
 #include "mep3_navigation/stuck_behavior/stuck_behavior.hpp"
-
+#include "pluginlib/class_list_macros.hpp"
 
 mep3_navigation::StuckBehavior::StuckBehavior() :
   nav2_behaviors::TimedBehavior<ActionT>()
 {
+  std::cout << "STUCK BEHAVIOR CONSTRUCTOR" << std::endl << std::endl;
+  RCLCPP_WARN(this->logger_, "STUCK BEHAVIOR CONSTRUCTOR!");
 }
 
 mep3_navigation::StuckBehavior::~StuckBehavior() = default;
@@ -19,3 +21,6 @@ nav2_behaviors::Status mep3_navigation::StuckBehavior::onCycleUpdate()
   RCLCPP_INFO(this->logger_, "onCycleUpdate successful!");
   return nav2_behaviors::Status::RUNNING;
 }
+
+PLUGINLIB_EXPORT_CLASS(mep3_navigation::StuckBehavior, nav2_core::Behavior)
+
