7435d4641c18e03793a5c77e7b695eba0bbdf2b2
Vladimir Vincan <vladimirvincan@gmail.com>
2023-04-18 01:46:38 +0200

    udp receives points and publishes to topic

--- mep3_hardware/test/UDP_communucation/udp_client.py
+++ mep3_hardware/test/UDP_communucation/udp_client.py
@@ -1,24 +1,54 @@
 import socket
 import select
+import rclpy
+import logging
+from rclpy.node import Node
+from mep3_msgs.msg import Scoreboard
 
-UDP_IP = "192.168.8.129"  # IP address of the UDP server (change this to the IP of your ESP32)
-UDP_PORT = 8888  # Port number of the UDP server (change this to the port number you used in your ESP32 code)
-BUFFER_SIZE = 1024  
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-counter = 0
-while (True):
-	MESSAGE = ("Hello, server! " + str(counter)).encode('utf-8')  # The message you want to send to the server
-
-	sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-
-	print("Sent message:", MESSAGE.decode())
-	counter += 1
-
-	ready, _, _ = select.select([sock], [], [], 0.1)  # Check if data is available to be read
-	if ready:
-		data, addr = sock.recvfrom(BUFFER_SIZE)
-		print("Received message:", data.decode())
+UDP_IP = "192.168.8.129"  # IP address of the UDP server
+UDP_PORT = 8888  # Port number of the UDP server
+BUFFER_SIZE = 1024
 
 
-sock.close()
+class ScoreboardSubscriber(Node):
+
+    def __init__(self):
+        super().__init__('scoreboard_subscriber')
+        self.publisher = self.create_publisher(Scoreboard, '/scoreboard', 10)
+        self.get_logger().info('Logging level: %d' % self.get_logger().get_effective_level())
+
+        # Create a UDP socket and listen for incoming data
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # self.sock.bind((UDP_IP, UDP_PORT))
+        # self.sock.setblocking(False)
+
+        timer_period = 1.0  # seconds
+        self.timer = self.create_timer(timer_period, self.publish_scoreboard)
+
+    def publish_scoreboard(self):
+        MESSAGE = ("Hello, server! ").encode('utf-8')
+        self.sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+        ready, _, _ = select.select([self.sock], [], [], 0.1)  # Check if data is available to be read
+        self.get_logger().info('Entered timer')
+        if ready:
+            data, addr = self.sock.recvfrom(BUFFER_SIZE)
+            print("Received message:", data.decode())
+
+            scoreboard_msg = Scoreboard()
+            scoreboard_msg.task = 'box'
+            scoreboard_msg.points = int(data)
+            self.publisher.publish(scoreboard_msg)
+            self.get_logger().info('Published scoreboard: task=%s, points=%d' % (scoreboard_msg.task, scoreboard_msg.points))
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = ScoreboardSubscriber()
+    rclpy.logging.set_logger_level('scoreboard_publisher', logging.DEBUG) # Set the logging level to DEBUG
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
