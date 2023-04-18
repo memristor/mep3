de39cc3f1d225de6d0cb072e03d1319a051af948
Vladimir Vincan <vladimirvincan@gmail.com>
2023-04-18 18:44:32 +0200

    firmware finished

--- firmware/box_v2/box_v2.ino
+++ firmware/box_v2/box_v2.ino
@@ -2,6 +2,8 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
 
 
 // Define the pins for the motors and drivers
@@ -29,6 +31,7 @@ Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT, &Wire, -1);
 
 // Initialize the sensor pin
 const int sensorPin = 36;
+int motor_enable = 1;
 
 // Initialize the object counter
 int objectCount = 0;
@@ -49,8 +52,10 @@ const unsigned long displayInterval = 1000;
 const char* ssid     = "memristor";
 const char* password = "ftnmemristor";
 
-// Variable to store the HTTP request
-String header;
+// UDP server parameters
+WiFiUDP udp;
+unsigned int localUdpPort = 8888;
+unsigned long UDP_TX_PACKET_MAX_SIZE = 32;
 
 
 void setup() {
@@ -65,12 +70,6 @@ void setup() {
     delay(1);
   }
 
-  // Set up the motor properties
-  stepper1.setMaxSpeed(maxSpeed);
-  stepper1.setAcceleration(acceleration);
-  stepper2.setMaxSpeed(1500);
-  stepper2.setAcceleration(1500);
-
   // Set up the OLED display
   display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
   display.clearDisplay();
@@ -82,8 +81,47 @@ void setup() {
   delay(2000);
   display.clearDisplay();
 
-  // block until received value is 1
+  // Connect to Wi-Fi network
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
   
+  // Start UDP server
+  if (!udp.begin(localUdpPort)) {
+    Serial.println("Failed to start UDP server");
+    while (1) {}
+  }
+  Serial.print("Local IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("UDP server started on port ");
+  Serial.println(localUdpPort);  
+
+  Serial.println("Waiting for 1 to start loop.");
+
+  // Wait for "1" to be received
+  bool received = false;
+  while (!received) {
+    if (udp.parsePacket()) {
+      char buffer[128];
+      Serial.print("Received flag: ");
+      Serial.println(buffer);
+      udp.read(buffer, 128);
+      buffer[1] = 0;
+      if (strcmp(buffer, "1") == 0) {
+        received = true;
+      }
+    }
+  }
+
+  // Set up the motor properties
+  stepper1.setMaxSpeed(maxSpeed);
+  stepper1.setAcceleration(acceleration);
+  stepper2.setMaxSpeed(1500);
+  stepper2.setAcceleration(1500);
+  
+  Serial.println("Starting loop.");
 }
 
 
@@ -107,26 +145,54 @@ void loop() {
       display.print(objectCount);
       display.display();
 
+      // send points via udp
+      char message[32];
+      sprintf(message, "%lu", objectCount);
+      udp.beginPacket(udp.remoteIP(), udp.remotePort());
+      udp.print(message);
+      udp.endPacket();
+      Serial.print("Sent message: ");
+      Serial.println(message);
+
       previousDisplayTime = currentTime;
     }
 
-  // Move the first motor continuously
-  if (!stepper1.distanceToGo()) 
-    {
-      stepper1.moveTo(-100000);
-    }
-  stepper1.run();
+  // Check for incoming UDP messages
+  int packetSize = udp.parsePacket();
+  if (packetSize) {
+    char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
+    udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+    motor_enable = packetBuffer[0] - '0';
+    Serial.print("Received message: ");
+    Serial.println(packetBuffer);
+  }
 
-  // Move the second motor back and forth by 20 degrees
-  static bool direction = true;
-  static int currentPosition = 0;
-  const int targetPosition = direction ? currentPosition + 20 : currentPosition - 20;
-  stepper2.moveTo(targetPosition);
-
-  if (abs(stepper2.distanceToGo()) < 10) 
-    {
-      currentPosition = targetPosition;
-      direction = !direction;
-    }
-  stepper2.run();
+  // check if motors should be turned off
+  if (motor_enable)
+  {
+    // Move the first motor continuously
+    if (!stepper1.distanceToGo()) 
+      {
+        stepper1.moveTo(-100000);
+      }
+    stepper1.run();
+  
+    // Move the second motor back and forth by 20 degrees
+    static bool direction = true;
+    static int currentPosition = 0;
+    const int targetPosition = direction ? currentPosition + 20 : currentPosition - 20;
+    stepper2.moveTo(targetPosition);
+  
+    if (abs(stepper2.distanceToGo()) < 10) 
+      {
+        currentPosition = targetPosition;
+        direction = !direction;
+      }
+    stepper2.run();    
+  }
+  else 
+  {
+    stepper1.enableOutputs();
+    stepper2.enableOutputs();
+  }
 }
---
+++ mep3_hardware/test/UDP_communucation/udp_client_basic_v2.py
@@ -0,0 +1,24 @@
+import socket
+import select
+
+UDP_IP = "192.168.8.129"  # IP address of the UDP server (change this to the IP of your ESP32)
+UDP_PORT = 8888  # Port number of the UDP server (change this to the port number you used in your ESP32 code)
+BUFFER_SIZE = 1024 
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+counter = 0
+while (True):
+    MESSAGE = (str(counter)).encode('utf-8')  # The message you want to send to the server
+
+    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+    print("Sent message:", MESSAGE.decode())
+    # counter += 1
+
+    ready, _, _ = select.select([sock], [], [], 0.1)  # Check if data is available to be read
+    if ready:
+        data, addr = sock.recvfrom(BUFFER_SIZE)
+        print("Received message:", data.decode())
+
+
+sock.close()
