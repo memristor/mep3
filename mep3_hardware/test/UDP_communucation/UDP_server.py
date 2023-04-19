f48f0b14185132f25bb17e6a85c66900165ad0b3
Marija Golubović <93673097+MarijaGolubovic@users.noreply.github.com>
2023-04-20 00:48:19 +0200

    UDP server (#266)
    * Add UDP client and server
    
    * working udp communication on esp32
    
    * udp receives points and publishes to topic
    
    * box v2 init
    
    * firmware finished
    
    * fix disable motor
    
    * update udp client
    
    * change score to points
    
    * timer on box_driver works
    
    * add box_driver to hardware launch file
    
    * change small to big
    
    * fix ip address. fix pinout for esp32
    
    ---------
    
    Co-authored-by: Vladimir Vincan <vladimirvincan@gmail.com>

---
+++ mep3_hardware/test/UDP_communucation/UDP_server.py
@@ -0,0 +1,28 @@
+import socket
+
+localIP     = "192.168.8.101"
+localPort   = 8888
+bufferSize  = 1024
+
+msgFromServer       = "12345"
+bytesToSend         = str.encode(msgFromServer)
+
+UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+UDPServerSocket.bind((localIP, localPort))
+print("UDP server up and listening")
+sum = 0
+while(True):
+    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+    message = bytesAddressPair[0]
+    address = bytesAddressPair[1]
+
+    clientMsg = "Message from Client:{}".format(message)
+    clientIP  = "Client IP Address:{}".format(address)
+    received_points = clientMsg[22:-1]
+
+    sum += int(received_points)
+
+    print("Received points: ",received_points,", current sum: ", sum)
+    print(clientIP)
+
+    UDPServerSocket.sendto(bytesToSend, address)
