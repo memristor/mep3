59ef2cc1d11728114437c6b168f4bcec050ce9ee
Marija Golubovic <marijagolubovic16@gmail.com>
2024-04-04 16:03:34 +0000

    fix startegu

--- mep3_hardware/mep3_hardware/cinch_driver.py
+++ mep3_hardware/mep3_hardware/cinch_driver.py
@@ -28,15 +28,15 @@ class CinchDriver(Node):
             Int8, '/match_start_status', QoSProfile(depth=1, reliability=ReliabilityPolicy.RELIABLE, durability=DurabilityPolicy.TRANSIENT_LOCAL))
         self.__can_bus = can.interface.Bus(channel='can0', bustype='socketcan', can_filters=filters)
 
-        self.lcd_publisher = self.create_publisher(Scoreboard, '/scoreboard', 10)
+        # self.lcd_publisher = self.create_publisher(Scoreboard, '/scoreboard', 10)
 
         self.ip_addreses = [UDP_IP_1, UDP_IP_2, UDP_IP_3]
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.start_msg = "1212"
         self.start_time = None
-        self.is_first_came = False
-        self.is_second_came = False
-        self.is_third_came = False
+        # self.is_first_came = False
+        # self.is_second_came = False
+        # self.is_third_came = False
         self.number_of_send_msgs = 0
     
     def wait_for_cinch_insertion(self):
@@ -71,7 +71,7 @@ class CinchDriver(Node):
                     MESSAGE = (self.start_msg).encode('utf-8')
 
                 MESSAGE = (self.start_msg).encode('utf-8')
-                while time.time() - self.start_time < 93:
+                while time.time() - self.start_time < 96:
                     for ip_address in self.ip_addreses:
                         self.sock.sendto(MESSAGE, (ip_address, UDP_PORT))
 
@@ -81,29 +81,29 @@ class CinchDriver(Node):
                 
                 
             
-                while time.time() - self.start_time < 91:
-                    data, addr = self.sock.recvfrom(BUFFER_SIZE)
-                    print(f"Received message from ip_address {ip_address}:", data.decode())
-                    if int(data.decode())==1 and  not self.is_first_came:
-                        scoreboard_msg = Scoreboard()
-                        scoreboard_msg.points = 15
-                        scoreboard_msg.task = 'SIMA'+str(ip_address)
-                        self.is_first_came  = True
-                        self.lcd_publisher.publish(scoreboard_msg)
+                # while time.time() - self.start_time < 91:
+                #     data, addr = self.sock.recvfrom(BUFFER_SIZE)
+                #     print(f"Received message from ip_address {ip_address}:", data.decode())
+                #     if int(data.decode())==1 and  not self.is_first_came:
+                #         scoreboard_msg = Scoreboard()
+                #         scoreboard_msg.points = 15
+                #         scoreboard_msg.task = 'SIMA'+str(ip_address)
+                #         self.is_first_came  = True
+                #         self.lcd_publisher.publish(scoreboard_msg)
                     
-                    if int(data.decode())==2 and  not self.is_second_came:
-                        scoreboard_msg = Scoreboard()
-                        scoreboard_msg.points = 10
-                        scoreboard_msg.task = 'SIMA'+str(ip_address)
-                        self.is_second_came  = True
-                        self.lcd_publisher.publish(scoreboard_msg)
+                #     if int(data.decode())==2 and  not self.is_second_came:
+                #         scoreboard_msg = Scoreboard()
+                #         scoreboard_msg.points = 10
+                #         scoreboard_msg.task = 'SIMA'+str(ip_address)
+                #         self.is_second_came  = True
+                #         self.lcd_publisher.publish(scoreboard_msg)
                     
-                    if int(data.decode())==3 and  not self.is_third_came:
-                        scoreboard_msg = Scoreboard()
-                        scoreboard_msg.points = 10
-                        scoreboard_msg.task = 'SIMA'+str(ip_address)
-                        self.is_third_came  = True
-                        self.lcd_publisher.publish(scoreboard_msg)
+                #     if int(data.decode())==3 and  not self.is_third_came:
+                #         scoreboard_msg = Scoreboard()
+                #         scoreboard_msg.points = 10
+                #         scoreboard_msg.task = 'SIMA'+str(ip_address)
+                #         self.is_third_came  = True
+                #         self.lcd_publisher.publish(scoreboard_msg)
         rclpy.spin_once(self, timeout_sec=0)
 
 
